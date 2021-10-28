49725fbe3e334cf453f2a8e42b7597f513d8fc9d
Mateo Matic <mateo.eyo@gmail.com>
2021-10-27 22:00:41 -0400

    Trying to add Max's awesome code to this program, but I'm not as successful :(

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace simple_DES
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             // Specify file path
@@ -16,13 +17,31 @@ namespace simple_DES
             // Read all from path into byte array
             string fileText = File.ReadAllText(path);
             string binaryString = ToBinary(ConvertToByteArray(fileText, Encoding.ASCII));
-            Console.WriteLine(binaryString);
+
+            int numOfBytes = binaryString.Length / 8;
+            byte[] bytes = new byte[numOfBytes];
+            for(int i = 0; i < numOfBytes; ++i)
+            {
+                bytes[i] = Convert.ToByte(binaryString.Substring(8 * i, 8), 2);
+            }
+            foreach (byte item in bytes)
+            {
+                byte leftHalf = GetLeftHalfByte(item);
+                byte rightHalf = GetRightHalfByte(item);
+
+                Console.WriteLine($"starting: {item}");
+                Console.WriteLine($"left: {leftHalf}");
+                Console.WriteLine($"right: {rightHalf}");
+                byte recombined = RecombineByte(leftHalf, rightHalf);
+                Console.WriteLine($"recombined: {recombined}");
+            }
         }
 
         /*
             *** FUNCTIONS ***
         */
 
+        // * Utility Functions *
         // ConvertToByteArray / ToBinary are used to convert some string into binary
         public static byte[] ConvertToByteArray(string str, Encoding encoding)
         {
@@ -33,5 +52,25 @@ namespace simple_DES
         {
             return string.Join("", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
         }
+
+        // Handle Byte shifting
+        static byte GetLeftHalfByte(byte input)
+        {
+            return (byte)(input >> 4);
+        }
+
+        static byte GetRightHalfByte(byte input)
+        {
+            return (byte)(input << 28 >> 28);
+        }
+
+        static byte RecombineByte(byte left, byte right)
+        {
+            return (byte)((left << 4) ^ (right));
+        }
+
+        // * Encryption Functions *
+
+        // * Decryption Functions *
     }
 }
