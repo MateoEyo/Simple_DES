a9752df3f726e27bfc2bb973fb99fe67b4449c2a
Mateo Matic <mateo.eyo@gmail.com>
2021-10-29 00:20:27 -0400

    I removed some previous code and figured out how to create the key with the rounds. Happy to stop here since it's midnight now and I'm tired.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
 using System.Text;
 
 namespace simple_DES
@@ -16,7 +15,18 @@ namespace simple_DES
 
             // Read all from path into byte array
             string fileText = File.ReadAllText(path);
+
+            // Convert all text from file into binary
             string binaryString = ToBinary(ConvertToByteArray(fileText, Encoding.ASCII));
+
+            string key = GetKey();
+
+            // Convert key to binary string
+            string binaryKey = String.Join(String.Empty,
+                key.Select(
+                    c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
+                )
+            );
 
             int numOfBytes = binaryString.Length / 8;
             byte[] bytes = new byte[numOfBytes];
@@ -27,19 +37,13 @@ namespace simple_DES
 
             Console.WriteLine($"fileText: {fileText}");
             Console.WriteLine($"binaryString: {binaryString}");
-            Console.WriteLine($"bytes: {Encoding.ASCII.GetString(bytes)}\n");
+            Console.WriteLine($"bytes: {Encoding.ASCII.GetString(bytes)}");
+            Console.WriteLine($"key: {key}");
+            Console.WriteLine($"binary key: {binaryKey}\n");
+            
+            string[] feistelKeys = GetKeyXORS(binaryKey);
 
-            foreach (byte item in bytes)
-            {
-                byte leftHalf = GetLeftHalfByte(item);
-                byte rightHalf = GetRightHalfByte(item);
-
-                Console.WriteLine($"starting: {item}");
-                Console.WriteLine($"left: {leftHalf}");
-                Console.WriteLine($"right: {rightHalf}");
-                byte recombined = RecombineByte(leftHalf, rightHalf);
-                Console.WriteLine($"recombined: {recombined}\n");
-            }
+            string ciphertext = EncryptPlainText(binaryString, feistelKeys);
         }
 
         /*
@@ -58,23 +62,60 @@ namespace simple_DES
             return string.Join("", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
         }
 
-        // Handle Byte shifting
-        static byte GetLeftHalfByte(byte input)
+        // * Key related functions
+        // Returns a hex string of 12
+        static string GetKey()
         {
-            return (byte)(input >> 4);
+            System.Random random = new System.Random();
+            int num = random.Next(256, 4095);
+            return num.ToString("X");
         }
 
-        static byte GetRightHalfByte(byte input)
+        // Returns a 3 element array of 4 bit keys used for SD_DES
+        static string[] GetKeyXORS(string key)
         {
-            return (byte)(input << 28 >> 28);
+            const int SIZE = 3;
+            string[] preFeistelRounds = new string[SIZE];
+            string[] postFeistelRounds = new string[SIZE];
+            
+            preFeistelRounds[0] = key.Substring(0, 4);
+            preFeistelRounds[1] = key.Substring(4, 4);
+            preFeistelRounds[2] = key.Substring(8, 4);
+
+            postFeistelRounds[0] = GetXORFromElements(preFeistelRounds[0], preFeistelRounds[1]);
+            postFeistelRounds[1] = GetXORFromElements(preFeistelRounds[1], preFeistelRounds[2]);
+            postFeistelRounds[2] = GetXORFromElements(preFeistelRounds[2], preFeistelRounds[0]);
+
+            return postFeistelRounds;
         }
 
-        static byte RecombineByte(byte left, byte right)
+        // Return 4 bit XOR element
+        static string GetXORFromElements(string first, string second)
         {
-            return (byte)((left << 4) ^ (right));
+            int i = 0;
+            string xorToReturn = String.Empty;
+
+            foreach (char c in first)
+            {
+                if (c == second[i])
+                {
+                    xorToReturn += '0';
+                } else {
+                    xorToReturn += '1';
+                }
+                i++;
+            }
+
+            return xorToReturn;
         }
 
         // * Encryption Functions *
+        static string EncryptPlainText(string plaintext, string[] keys)
+        {
+            
+
+            return null;
+        }
 
         // * Decryption Functions *
     }
